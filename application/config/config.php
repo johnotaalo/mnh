678f5b1dc13e64d4b6a6d3880fd47be8747d0367
Catherine <kyalocatherine@gmail.com>
2013-06-07 10:37:37 +0300

    My commit

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://'.$_SERVER['SERVER_NAME'].'/mnh/';
+$config['base_url']	= 'http://'.$_SERVER['SERVER_NAME'].':88/mnh/';
 
 /*
 |--------------------------------------------------------------------------
