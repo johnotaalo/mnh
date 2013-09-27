7bd043340bf650cd1a8b87c90682dab32faa8b56
Rufus Ngugi Mbugua <mbuguarufus@gmail.com>
2013-09-27 08:49:19 +0300

    Preparing PDF Functionality

--- application/controllers/c_analytics.php
+++ application/controllers/c_analytics.php
@@ -946,5 +946,20 @@ class C_Analytics extends MY_Controller {
 		//var_dump($options);
 		echo $options;
 	}
+//Load PDF
+public function loadPDF() {
+		$stylesheet = ('');
+		$html = ('Test');
+		$this -> load -> library('mpdf');
+		$this -> mpdf = new mPDF('', 'A4-L', 0, '', 15, 15, 16, 16, 9, 9, '');
+		$this -> mpdf -> SetTitle('DCAH Assessment Tool');
+		$this -> mpdf -> SetHTMLHeader('<em>DCAH Assessment Tool</em>');
+        $this -> mpdf -> SetHTMLFooter('<em>DCAH Assessment Tool</em>');
+		$this -> mpdf -> simpleTables = true;
+		$this -> mpdf -> WriteHTML($stylesheet, 1);
+		$this -> mpdf -> WriteHTML($html, 2);
+		$report_name = 'DCAH Assessment Tool' . ".pdf";
+		$this -> mpdf -> Output($report_name, 'D');
 
+	}
 }
