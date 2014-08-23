e6c3284a7044457eb649f9232a8ac33f13d9b3a9
Rufus Mbugua <mbuguarufus@gmail.com>
2014-08-23 08:27:02 +0300

    System Home Changes

--- js/core.js
+++ js/core.js
@@ -319,7 +319,10 @@ function getCountyData(base_url,county, survey_type, survey_category){
             $('#finished .digit').text(obj[0].reported);
             $('#started .digit').text(obj[0].unfinished);
             $('#not_started .digit').text(obj[0].notstarted);
-            $('#county_progress').text((obj[0].reported / obj[0].actual * 100) + ' %');
+            percentage = Math.round((obj[0].reported / obj[0].actual * 100),2);
+            $('#county_progress .progress-bar').text(percentage+'%' );
+             $('#county_progress .progress-bar').attr('aria-valuenow',percentage );
+             $('#county_progress .progress-bar').css('width',percentage+'%' );
             url = base_url + 'c_analytics/setActive/' + county + '/' + survey_type + '/' + survey_category;
             $('#load_analytics').attr('data-url', url);
             new_url = base_url + 'c_analytics/getCountyReportingSummary/'+survey + '/' + survey_category;
